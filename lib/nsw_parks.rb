339080baec8243d52b477117735ab98e3e872694
apalski <annette.drapalski@gmail.com>
2017-03-13 14:58:35 +1100

    Update comments for class NSWParks

--- lib/nsw_parks.rb
+++ lib/nsw_parks.rb
@@ -9,19 +9,21 @@ class NSWParks
 	def initialize (name, park_url)
 		@name = name
 		@park_url = park_url
-		@@all << self  # Add each new NSW National Park as it is created
+		@@all << self  # Add each new NSW National Park to the collection as it is created
 	end
 
 	def self.all
 		@@all  # Access all created NSW National Parks
 	end
 
+	# Create new NSW National Parks from the National Parks webstie
    	def self.new_park
    		page = Nokogiri::HTML(open("http://www.nationalparks.nsw.gov.au/conservation-and-heritage/national-parks"))		
 	    park = page.css("#content__inner .dynamicListing li a")
 	    park.collect {|a| new(a.text.strip, a.attribute("href").value)}
   	end 	
 
+  	# Allow user to access an overview of a NSW National Park
   	def self.park_overview(park_no)
   		# Select the park from the list in .CLI
   		park = Nokogiri::HTML(open("#{@@all[park_no - 1].park_url}"))
@@ -48,12 +50,15 @@ class NSWParks
    	  end
    end
 
+   # Show the Regions that NSW National Parks are listed within
    def self.nsw_regions
 		regions = Nokogiri::HTML(open("http://www.nationalparks.nsw.gov.au/visit-a-park"))
 		list = regions.css("#mainParkNavJump ul li")
+		# Put out the list of Regions numerically
 		list.collect.with_index(1) {|a,i| puts "#{i}. #{a.children.text}"}
    end
 
+   # Assign a region to each NSW National Park in the collection
    def self.park_region
   		@@all.each.with_index do |region, i|
   			park = Nokogiri::HTML(open("#{@@all[i].park_url}"))
