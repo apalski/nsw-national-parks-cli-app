528cdc3a1cc5a02a0cfdef2dfa30924ba1a83a0a
apalski <annette.drapalski@gmail.com>
2017-03-13 15:08:32 +1100

    Create new branch CLI and begin coding CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,24 @@
-
 require '../config/environment'
 require_relative '../lib/nsw_parks'
 
 class CLI
 
+  def call
+    puts "Welcome to the NSW National Parks Information App"
+    puts ""
+    puts "All NSW National Parks are listed below:"
+    puts ""
+    park_list
+  end
+
+  def park_list
+    NSWParks.new_park
+    list = NSWParks.all.each.with_index(1) do |a,i| 
+      puts "#{i}. #{a.name}"
+    end 
+  end 
+
 
 end
 
+#CLI.park_list
